7dc1b7e8a14e4300b971db438d83217036fa6673
VJ <44183618+gitvijay2010@users.noreply.github.com>
2022-06-14 13:32:55 +0530

    Update MenuItem.php

--- app/Models/MenuItem.php
+++ app/Models/MenuItem.php
@@ -9,4 +9,20 @@ use Illuminate\Database\Eloquent\Model;
 class MenuItem extends Model
 {
 
+  //protected $table = 'menu_items';
+  
+	public function parent() {
+
+        return $this->hasOne(MenuItem::class, 'id', 'parent_id');
+    }
+
+    public function children() {
+
+        return $this->hasMany(MenuItem::class, 'parent_id', 'id');
+    }  
+
+    public static function tree() {
+
+        return static::with(implode('.', array_fill(0, 4, 'children')))->where('parent_id', '=', NULL)->get();
+    }
 }
